8ef9a684735f41e78c148c8d898183dd8266d728
ユãtkm015 <takumi200015@yahoo.co.jp>
2020-08-12 11:04:37 +0000

    update

--- db/migrate/20200725063339_create_favorites.rb
+++ db/migrate/20200725063339_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.references :customer, foreign_key: true
-      t.references :post, foreign_key: true
+      t.references :customer
+      t.references :post
 
       t.timestamps
     end
