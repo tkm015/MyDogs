a10744cf07641e91ff3adfcb3cf8894724d3180b
tkm015 <takumi200015@yahoo.co.jp>
2020-08-18 20:42:33 +0900

    update

--- app/controllers/public/dogs_controller.rb
+++ app/controllers/public/dogs_controller.rb
@@ -51,7 +51,7 @@ class Public::DogsController < ApplicationController
   def set_side
     @new_posts = Post.order('created_at DESC').limit(9)
     @popular_tags = Post.tag_counts_on(:tags).order('count DESC').limit(10)
-    @dog_ids = Relationship.group('dog_id').order('count_all DESC').limit(2).count.keys
+    @dog_ids = Relationship.group('dog_id').order('count_all DESC').limit(5).count.keys
   end
 
   def dog_params
