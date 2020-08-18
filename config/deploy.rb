71fb32d888b6661421ae4ae5b038cc0f8d381676
tkm015 <takumi200015@yahoo.co.jp>
2020-08-18 20:01:13 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:tkm015/MyDogs.git"
 set :deploy_to, "/home/ec2-user/MyDogs"
 set :rbenv_ruby, '2.5.7'
 set :linked_files, %w{config/master.key .env}
-append :linked_dirs, "log", "public/system", "tmp"
+append :linked_dirs, "log", "public/system", "public/uploads", "tmp"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
